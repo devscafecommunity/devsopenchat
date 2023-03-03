c84154131d4bf00d2ca634cef387e7d4e19cc851
Pedro Kaleb! <73436445+LyeZinho@users.noreply.github.com>
2023-03-03 11:54:26 +0000

    Update index.js

--- index.js
+++ index.js
@@ -84,6 +84,24 @@ let commands = {
     "usage": "/giphy <search>",
     "function": async (msg, args) => {
       // http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=YOUR_API_KEY&limit=1
+      /*
+      Return:
+      {
+        "data": [
+          {
+            "type": "gif",
+            "id": "MeIucAjPKoA120R7sN",
+            "url": "https://giphy.com/gifs/MeIucAjPKoA120R7sN", <- What we want
+            "slug": "MeIucAjPKoA120R7sN",
+            "bitly_gif_url": "https://gph.is/g/4AykB7O",
+            "bitly_url": "https://gph.is/g/4AykB7O",
+            "embed_url": "https://giphy.com/embed/MeIucAjPKoA120R7sN",
+            "username": "bluesbear",
+            "source": "",
+            "title": "Happy Cheering GIF by bluesbear",
+            "rating": "g",
+            ...
+      */
       let result = await new Promise((resolve, reject) => {
 
         let url = `http://api.giphy.com/v1/gifs/search?q=${args.join("+")}&api_key=${GIPHY_KEY}&limit=1`;
@@ -91,7 +109,7 @@ let commands = {
         request(url, { json: true }, (err, res, body) => {
           if (err) { reject(err); }
           let rer = `
-            <img src="${body.data[0].url ?? notresponse}">
+            <img src="${body.data[0]?.url ?? notresponse}">
           `;
           resolve(rer);
         }
